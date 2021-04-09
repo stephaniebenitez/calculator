bd690365b6ecc79740410d280d817ca64ac40c24
Gordon Hui <randomizer@macbook-pro.lan>
2021-04-08 22:42:09 -0400

    javascript started

--- cal.js
+++ cal.js
@@ -1,1 +1,49 @@
+class Calculator {
+  constructor(previousEntry, currentEntry) {
+    this.previousEntry = previousEntry;
+    this.currentEntry = currentEntry;
+    this.clear();
+  }
+  clear() {
+    this.currentOperant = '';
+    this.perviousOperant = '';
+    this.operation = undefined;
+  }
 
+  delete() {
+    //remove 1 number
+  }
+
+  appendNumber(number) {
+    //whenever press number adds it on
+  }
+
+  chooseOperation(operation) {
+    //whatever press for operation add it into the image
+  }
+
+  compute() {
+    //does the calculator
+  }
+
+  updateDisplay() {
+    this.currentEntry.innerText = this.currentEntry;
+  }
+}
+
+const numberButtons = document.querySelectorAll('#number');
+const operationButtons = document.querySelectorAll('#operator');
+const equalButton = document.querySelector('#equal');
+const deleteButton = document.querySelector('#delete');
+const allClearButton = document.querySelector('#allclear');
+const previousEntry = document.querySelector('#previous-entry');
+const currentEntry = document.querySelector('#current-entry');
+
+const calculator = new Calculator(previousEntry, currentEntry);
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
